630efb5b68a334cca1ebc32e048497f65bfffe41
allisonjungg <allisonjungg@gmail.com>
2018-07-13 17:13:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector){
   return document.querySelector(selector)
 }
 function nestedTarget() {
-  return document.getElementById('nested').querySelector()
+  return document.getElementById('nested').querySelector('div')
 }
