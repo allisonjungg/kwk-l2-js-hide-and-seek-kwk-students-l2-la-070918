b7e77601e2b71f86c6c0c56fc243931e36f08a61
allisonjungg <allisonjungg@gmail.com>
2018-07-13 17:35:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('div')
 }
 function increaseRankBy(n){
-  const lis = document.('').querySelector
+  const lis = document.querySelectorAll('ul.ranked-list')
   for (let i=0; i < lis.length; i++) {
-    lis[i].ranked-list = 
+    lis[i].innerHTML = (i+1).toString()
   }
   return document.querySelector(".ranked-list ")
 }
