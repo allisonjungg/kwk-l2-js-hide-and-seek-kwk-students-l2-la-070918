f9270ace189f6f35e74af7d97a832eb7ec61b554
allisonjungg <allisonjungg@gmail.com>
2018-07-13 17:32:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('div')
 }
 function increaseRankBy(n){
+  const lis = document.('').querySelector
   for (let i=0; i < lis.length; i++) {
     lis[i].ranked-list = 
   }
